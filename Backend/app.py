58a090e3bfd1f59ab1b70757f64a771bef34627d
andrewpratheepan <andrew.pratheepan@gmail.com>
2020-02-08 15:38:01 -0500

    initial backend

---
+++ Backend/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from waitress import serve
+from logic import operations
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return 'Hello World!'
+
+# @app.route('/generate', methods=['POST'])
+# def generate():
+#     return {'link': operations.generate_extension()}
+#
+# @app.route('/poll', methods=['POST'])
+# def poll():
+#     return operations.poll(request.get_json())
+#
+# @app.route('/vote', methods=['POST'])
+# def vote():
+#     return operations.vote(request.get_json())
+#
+# @app.route('/getPoll', methods=['POST'])
+# def get_poll():
+#     return operations.get_poll(request.get_json())
+
+if __name__ == '__main__':
+    # app.run()
+    serve(app, host='0.0.0.0', port=80)
